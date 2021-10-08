ce0026c39da9da13874c7f618a0e74c5b2f60a5c
adm134 <adm134@yandex.ru>
2021-10-08 13:25:27 +0300

    Изменение формата вывода сравнения.

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -11,6 +11,8 @@ import java.io.File;
 import java.io.OutputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -29,26 +31,40 @@ public class Differ {
         mapAll.putAll(map1);
         mapAll.putAll(map2);
 
-        ObjectNode rootNode = mapper.createObjectNode();
-        OutputStream outputStream = new ByteArrayOutputStream();
+//        ObjectNode rootNode = mapper.createObjectNode();
+//        OutputStream outputStream = new ByteArrayOutputStream();
 
+        LinkedHashMap <String,String> mapOut = new LinkedHashMap<>();
         for (Map.Entry entry : mapAll.entrySet()) {
             String key = (String) entry.getKey();
             if (map2.get(key) == null) {
-                rootNode.put("- " + key, entry.getValue().toString());
+                mapOut.put("- " + key, entry.getValue().toString());
             } else if (map1.get(key) == null) {
-                rootNode.put("+ " + key, entry.getValue().toString());
+                mapOut.put("+ " + key, entry.getValue().toString());
             } else if (map2.get(key).equals(map1.get(key))) {
-                rootNode.put(key, entry.getValue().toString());
+                mapOut.put("  " +key, entry.getValue().toString());
             } else if (!map2.get(key).equals(map1.get(key))) {
-                rootNode.put("- " + key, map1.get(key).toString());
-                rootNode.put("+ " + key, map2.get(key).toString());
+                mapOut.put("- " + key, map1.get(key).toString());
+                mapOut.put("+ " + key, map2.get(key).toString());
             }
         }
-        ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
-        writer.writeValue(outputStream, rootNode);
+//        ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+//        writer.writeValue(outputStream, rootNode);
 
-        return outputStream.toString();
+        return writeOutString(mapOut);
 
+    }
+    static String writeOutString(LinkedHashMap <String,String> mapOut){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{\n");
+        for (Map.Entry <String,String> entry : mapOut.entrySet()) {
+            stringBuilder.append("  ");
+            stringBuilder.append(entry.getKey());
+            stringBuilder.append(": ");
+            stringBuilder.append(entry.getValue());
+            stringBuilder.append("\n");
+        }
+        stringBuilder.append("}\n");
+        return stringBuilder.toString();
     }
 }
