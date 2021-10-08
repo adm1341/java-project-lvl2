fba99cf56acb3d8d77bb1a8b57a04185b3ef742a
adm134 <adm134@yandex.ru>
2021-10-08 09:02:36 +0300

    Добавление справки и импорт  picocli.CommandLine

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,27 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import static java.lang.System.out;
+
+
+@Command(
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference.")
 public class App {
+    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.", help = true)
+    boolean help;
+
+    @Option(names = {"-V", "--version"}, description = "Print version information and exit.", help = true)
+    boolean version;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final App main = CommandLine.populateCommand(new App(), args);
+        if (main.help) {
+            CommandLine.usage(main, out, CommandLine.Help.Ansi.AUTO);
+        }
     }
+
 }
